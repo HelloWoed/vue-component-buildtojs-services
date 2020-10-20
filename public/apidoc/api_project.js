2e2f2e68057060c8784efb29e033baddc82522f9
zhangxi <zhangxip@dcits.com>
2020-10-20 11:25:38 +0800

    假如文档自动化生成

---
+++ public/apidoc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "前端自助构建平台APi",
+  "version": "0.1.0",
+  "description": "前端自助构建平台接口文档",
+  "title": "前端自助构建平台",
+  "url": "http://localhost:3000",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-10-20T03:22:05.500Z",
+    "url": "https://apidocjs.com",
+    "version": "0.25.0"
+  }
+});
--- routes/files/getFile.js
+++ routes/files/getFile.js
@@ -3,7 +3,24 @@ var router = express.Router();
 var path = require('path');
 const fs = require('fs');
 
-/* GET home page. */
+/**
+ * @api {get} /files/getFile 获取组件文件
+ * @apiDescription 获取组件文件
+ * @apiName getFile
+ * @apiGroup File
+ * @apiParam {string} fileName 文件名
+ * @apiSuccess {json} result
+ * @apiSuccessExample {json} Success-Response:
+ *  {
+ *      "success" : "true",
+ *      "result" : {
+ *          "name" : "loginName",
+ *          "password" : "loginPass"
+ *      }
+ *  }
+ * @apiSampleRequest http://localhost:3000/api/user/submit-login
+ * @apiVersion 1.0.0
+ */
 router.get('/', function(req, res, next) {
     let fileName = req.query.name;
     let filePath = path.join(__dirname,`../../public/components/${fileName}.js`);
